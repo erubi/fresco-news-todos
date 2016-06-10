9db593956a6673c2411e6e54e36094e4245c1d82
Evan Rubinton <evanrubinton@gmail.com>
2016-06-10 12:45:28 -0400

    add dev deps, webpack config, server js file

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    './src/index.js',
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'src'),
+        loader: 'react-hot!babel',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader!postcss-loader',
+      },
+    ],
+  },
+  postcss() {
+    return [
+      require('postcss-import')({ path: ['node_modules', './src'], addDependencyTo: webpack}),
+      require('autoprefixer'),
+      require('postcss-normalize'),
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.scss'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './',
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({ 'process.env.SERVER_URL': JSON.stringify('http://127.0.0.1:3000') }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+  ],
+};
+
